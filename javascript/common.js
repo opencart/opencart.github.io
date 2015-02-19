59794bbf87903ebefe0089ae4f61f5794580fbe2
Daniel Kerr <webmaster@opencart.com>
2015-02-20 02:43:56 +0800

    jquery

--- javascript/common.js
+++ javascript/common.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	$('#menu .parent').on('click', function(e) {
 		e.preventDefault();
 		
+		alert('hi');
+		
 		$(this).parent().slideToggle(400, function() {
 			$(this).addClass('open');
 		});
