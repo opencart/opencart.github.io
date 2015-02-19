463c1793646b065e79f5ea6065b7a1b6127a043a
Daniel Kerr <webmaster@opencart.com>
2015-02-20 03:11:42 +0800

    test

--- javascript/common.js
+++ javascript/common.js
@@ -2,8 +2,6 @@ $(document).ready(function() {
 	$('#menu .parent').on('click', function(e) {
 		e.preventDefault();
 		
-		alert('hi');
-		
 		$(this).parent().slideToggle(400, function() {
 			$(this).addClass('open');
 		});
