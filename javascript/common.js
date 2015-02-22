69442c5e885c2c1a38058b4038513a791c251017
Daniel Kerr <webmaster@opencart.com>
2015-02-23 00:32:52 +0800

    docs

--- javascript/common.js
+++ javascript/common.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 	$('#menu .parent').on('click', function(e) {
 		e.preventDefault();
 		
-		$(this).parents('li').siblings().removeClass('open');
-		
 		if (!$(this).parent().hasClass('open')) {
 			$(this).parent().addClass('open');
 		} else {
 			$(this).parent().removeClass('open');
 		}
+		
+		$(this).parents('li').siblings().removeClass('open');
 	});
 	
 	$('#menu a[href="' + String(document.location).substr($('base').attr('href').length) + '"]').parents('li').addClass('open');
