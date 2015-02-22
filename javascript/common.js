8f4ee5b7dd8a63cea2b898e591a21950223bbc03
Daniel Kerr <webmaster@opencart.com>
2015-02-23 00:16:07 +0800

    docs

--- javascript/common.js
+++ javascript/common.js
@@ -2,14 +2,14 @@ $(document).ready(function() {
 	$('#menu .parent').on('click', function(e) {
 		e.preventDefault();
 		
+		$('#menu .parent').not(this).removeClass('open');
+		
 		if (!$(this).parent().hasClass('open')) {
 			$(this).parent().addClass('open');
 		} else {
 			$(this).parent().removeClass('open');
 		}
 	});
-	
-	//alert(String(document.location).substr($('base').attr('href').length));
 	
 	$('#menu a[href="' + String(document.location).substr($('base').attr('href').length) + '"]').parents('li').addClass('open');
 });
