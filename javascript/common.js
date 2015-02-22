287830af8b75ded678f71e0b6cb66295a521f075
Daniel Kerr <webmaster@opencart.com>
2015-02-22 22:41:57 +0800

    docs

--- javascript/common.js
+++ javascript/common.js
@@ -1,27 +1,3 @@
-function getURLVar(key) {
-	var value = [];
-
-	var query = String(document.location).split('?');
-
-	if (query[1]) {
-		var part = query[1].split('&');
-
-		for (i = 0; i < part.length; i++) {
-			var data = part[i].split('=');
-
-			if (data[0] && data[1]) {
-				value[data[0]] = data[1];
-			}
-		}
-
-		if (value[key]) {
-			return value[key];
-		} else {
-			return '';
-		}
-	}
-}
-
 $(document).ready(function() {
 	$('#menu .parent').on('click', function(e) {
 		e.preventDefault();
@@ -35,12 +11,14 @@ $(document).ready(function() {
 		$(this).parent().find(' > ul').slideToggle(400);
 	});
 	
-	var query = String(document.location).split('?');
+	var url = String(document.location);
 	
 	if (query[1]) {
-		alert(query[1]);
-		$('#menu a[href="' + query[1] + '"].parent').parentsUntil('#menu', function(element) {
-			$(element).addClass('open');
-		});
+		alert(url.substr(0, $('base').attr('href').length));
+		
+		
+		//$('#menu a[href="' + query[1] + '"].parent').parentsUntil('#menu', function(element) {
+		//	$(element).addClass('open');
+		//});
 	}
 });
